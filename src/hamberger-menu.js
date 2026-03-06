654ca962ed0405bf8a0ec001e5fb2b9bea7d76e7
radiann9801 <snine9801@gmail.com>
2026-03-07 08:50:29 +0900

    ハンバーガーメニュー更新
    - そういえばリンク一覧が更新まだだった
    - ハンバーガーメニューがリンク一覧であることを明示するようテンプレートを改変

--- src/hamberger-menu.js
+++ src/hamberger-menu.js
@@ -4,20 +4,26 @@ Vue.createApp({})
 	.component("hamberger-menu", {
 		template: `<!--ハンバーガーメニューのボタン-->
 		<div class="hamburger_btn" v-on:click='ActiveBtn=!ActiveBtn'>
-			<span class="line line_01" v-bind:class="{'btn_line01':ActiveBtn}"></span>
-			<span class="line line_02" v-bind:class="{'btn_line02':ActiveBtn}"></span>
-			<span class="line line_03" v-bind:class="{'btn_line03':ActiveBtn}"></span>
+			<h2>リンク一覧
+				<div class="hamburger_icon">
+					<span class="line line_01" v-bind:class="{'btn_line01':ActiveBtn}"></span>
+					<span class="line line_02" v-bind:class="{'btn_line02':ActiveBtn}"></span>
+					<span class="line line_03" v-bind:class="{'btn_line03':ActiveBtn}"></span>
+				</div>
+			</h2>
 		</div>
 		<!--サイドバー-->
 		<transition name="menu">
 			<div class="menu" v-show="ActiveBtn">
 				<ul>
 					<li><a href="/index.html">サイトトップ</a></li>
-					<li><a href="https://database.numbertales-radiann.net/">創作ガイドライン/Creation Guidelines</a></li>
-					<li><a href="http://www.numbertales-radiann.com">ナンバーテールズ公式サイト</a></li>
-					<li><a href="https://www.pixiv.net/users/44375569">原作者公式pixiv</a></li>
-					<li><a href="https://radiann-kswg.fanbox.cc/">原作者公式pixivFANBOX</a></li>
-					<li><a href="https://radiann-kswg.booth.pm/">原作者公式BOOTH</a></li>
+					<li><a href="https://database.numbertales-radiann.net/">創作データベース/Creation Databases</a></li>
+					<li><a href="https://www.numbertales-radiann.com/">ナンバーテールズ公式サイト/NumberTales Official</a></li>
+					<li><a href="https://github.com/radiann-kswg">GitHub</a></li>
+					<li><a href="https://www.pixiv.net/users/44375569/">pixiv</a></li>
+					<li><a href="https://radiann-kswg.fanbox.cc/">pixivFANBOX</a></li>
+					<li><a href="https://radiann-kswg.booth.pm/">BOOTH</a></li>
+					<li><a href="https://misskey.io/@RadianN_kswg@radiann6631.net/">Misskey(個人/Personal)</a></li>
 				</ul>
 			</div>
 		</transition>`,
